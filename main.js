300e09fc197599d812440742c68c79c83a4cc598
Florencia <flxrenciaemilia@gmail.com>
2020-11-16 16:33:55 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function() {
+    // $(document).draw();
+    $('#content').animate({
+        opacity:1,
+        marginTop:'0',
+    }, 800);
+    $('h2').click(function() {
+        $(this).next('.subtext').slideToggle('fast');
+        $(this).children('.hex').toggleClass('moved');
+    })
+});
